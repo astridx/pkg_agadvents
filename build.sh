711fe209ba57dc4f0959c5bca5b4b32f29ad6591
astridx <info@astrid-guenther.de>
2021-04-27 11:17:54 +0200

    wip

---
+++ build.sh
@@ -0,0 +1,2 @@
+docker exec -it --user 1000 -w /home/astrid/git/joomla-development/pkg_agadvents/ ag-joomla-docker_php74_mysql ./vendor/bin/robo build
+docker exec -it --user 1000 -w /home/astrid/git/joomla-development/pkg_agadvents/j4/pkg_agadvents/ ag-joomla-docker_php74_mysql ./vendor/bin/robo build
